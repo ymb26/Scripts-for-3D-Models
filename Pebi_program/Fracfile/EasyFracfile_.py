cdee85ad283f774f8cbac3d5f6b974058d2eefab
ymb26 <94688150+ymb26@users.noreply.github.com>
2023-07-26 17:41:16 +0300

    Add fracfile maker in easy var

--- Pebi_program/Fracfile/EasyFracfile_.py
+++ Pebi_program/Fracfile/EasyFracfile_.py
@@ -1,13 +1,7 @@
-path_infile = r'C:\1\1_Field\Multi_var_2\18_10_years_project_well\project_wells\WELLTRACK_2.txt'
-path_outfile = r'C:\1\1_Field\Multi_var_2\18_10_years_project_well\project_wells\FRACFILE_new2.txt'
-
-#path_infile = "C:\\1\\1_Field\\OPR\\WELLTRACK_2.txt"
-#path_outfile = "C:\\1\\1_Field\\OPR\\FRACFILE_new2.txt"
-
+path_infile = r'C:\1\1_Field\Multi_var_2\23_MVR_2_case\WELLTRACKS\WELLTRACK_longitudinal.txt'
+path_outfile = r'C:\1\1_Field\Multi_var_2\23_MVR_2_case\Fracfiles\Fracfile_300_75_standart_12_longitudinal.txt'
 
 def writeFractuleFile(MD, number_well, path_outfile):
-    #list_of_Ach2 = ["3-6", "3-7", "3-8", "3-9"]
-    #print(number_well)
     out_file = open(path_outfile, 'a')
     i = 0
     while i < len(MD):
@@ -20,9 +14,6 @@ def writeFractuleFile(MD, number_well, path_outfile):
         else:
             out_file.write(str(round(MD[i], 1) + 0.5))
 
-        #if number_well in list_of_Ach2:
-        #    out_file.write("\t\'2.txt\'\t/\n")
-        #else:
         out_file.write("\t\'%s.txt\'\t/\n" % number_well)
         i += 1
 
@@ -39,15 +30,11 @@ def readInFile(path_infile, path_outfile):
     MD = list()
     #
     while (i < len(lines)):
-        #print(lines[i])
         if len(lines[i]) > 0 and lines[i][0] == "WELLTRACK":
             number_well = lines[i][1]
-            #print(number_well)
         elif len(lines[i]) == 4:
             MD.insert(0, float(lines[i][3]))
-            #print(MD)
         elif (len(lines[i]) > 0 and lines[i][0] == "/"):
-            #print(lines[i][0])
             writeFractuleFile(MD, number_well, path_outfile)
             MD.clear()
 
