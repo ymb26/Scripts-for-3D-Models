919bcd897b6d606b291abcb0a32121fa095d0659
ymb26 <94688150+ymb26@users.noreply.github.com>
2023-07-27 18:40:45 +0300

    Add two files: 1) inc (excel) to welltrack 2) Rewriter history in recu to schedule

---
+++ Pebi_program/Fracfile/IncToWellTrack.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import chardet
+import openpyxl
+import numpy as np
+import os
+
+
+def makeWellTrack(path_in, path_out):
+    #path_in = r'C:\1\2_Work_files\SPD\MVR\test.xlsx'
+    #path_out = r'C:\1\2_Work_files\SPD\MVR\ForGDM\WellTrac_300_m_12_stad.txt'
+
+    file_out = open(path_out, 'w')
+    df = pd.read_excel(path_in, header=None)
+
+    for row in df.itertuples(index=False):
+        if 'MD' in row:
+            df.columns = list(row)
+            df.rename(columns={df.columns[0]: 'Stade'}, inplace=True)
+            break
+    for idx, row in df.iterrows():
+        if row['MD'] == 0:
+            file_out.write("WELLTRACK %s\n" % df.iloc[idx - 1]['Stade'])
+        elif row['MD'] == "MD":
+            continue
+        #elif row['Stade'] == 1:
+        #    file_out.write("%f\t%f\n/\n\n" % (row['X'], row['Y']))       ##
+        elif np.isnan(row['Stade']):
+            continue
+        #if row['Stade'] == 1:
+        #    file_out.write("%f\t%f\t%f\t%f\n" % (row['X'], row['Y'], -1 * row['Z'], row['MD'] - 0.5))            ## write coord
+        #    file_out.write("/\n\n")       ##
+        #elif df.iloc[idx - 1]['MD'] == 0:
+        #    file_out.write("%f\t%f\t%f\t%f\n" % (row['X'], row['Y'], -1 * row['Z'], row['MD'] + 0.5))            ## write coord
+        #else:
+        #    file_out.write("%f\t%f\t%f\t%f\n" % (row['X'], row['Y'], -1 * row['Z'], row['MD']))
+        if row['Stade'] == 1:
+            file_out.write("%f\t%f\t%f\t%f\n" % (row['X'], row['Y'], -1 * row['Z'], row['MD']))
+            file_out.write("/\n\n")
+        else:
+            file_out.write("%f\t%f\t%f\t%f\n" % (row['X'], row['Y'], -1 * row['Z'], row['MD']))
+    file_out.close()
+    #############################################################
+
+
+def findAllFiles():
+    path_in = r'C:\1\1_Field\Multi_var_2\23_MVR_2_case'
+    path_out = r'C:\1\1_Field\Multi_var_2\23_MVR_2_case\WELLTRACKS'
+    count = 1
+    for root, dirs, files in os.walk(path_in):
+        for file in files:
+            if file.endswith(".xlsx"):
+                print(count)
+                count += 1
+                name = str(os.path.join(root, file))[str(os.path.join(root, file)).rfind('\\') + 1:-5]
+                print(path_out + '\WELLTRACK_' + name)
+                makeWellTrack(os.path.join(root, file), path_out + '\WELLTRACK_' + name + '.txt')
+
+
+
+if __name__ == "__main__":
+    findAllFiles()
---
+++ Pebi_program/History_recu_to_schedule/history_work_recu_to_schedule.py
@@ -0,0 +1,37 @@
+import pandas as pd
+#import openpyxl
+#import webbrowser
+#import os
+from datetime import datetime
+
+
+def fRoanding(number):
+    return "{:.2f}".format(number)
+
+##Macos
+#in_path = r'/Users/yurabaronov/Desktop/PHYSGEO/19_iter/HIST2.txt'
+#out_path = r'/Users/yurabaronov/Desktop/PHYSGEO/HIST_rewrite.txt'
+
+##Windows
+in_path = r'C:\1\4_Scripts\HIST.txt'
+out_path = r'C:\1\4_Scripts\HIST_schedule.txt'
+
+df = pd.read_csv(in_path, delimiter='\t', names=['WELL', 'DD.MM.YYYY', 'QOIL', 'QWAT', 'BHP', 'WEFA', 'QWIN', 'QGAS'])
+df['DD.MM.YYYY'] = pd.to_datetime(df['DD.MM.YYYY'], dayfirst=True).dt.date
+df2 = df.sort_values(by='DD.MM.YYYY', ascending=True)
+df2['flag'] = df2['DD.MM.YYYY'].duplicated()
+for row in df2.itertuples():
+    print(row)
+file_out = open(out_path, 'w')
+
+for index, row in df2.iterrows():
+    if not row['flag']:
+        file_out.write("DATES\n" + str(row['DD.MM.YYYY'].day) + ' ' + str(row['DD.MM.YYYY'].strftime("%B").upper()[:3]) + ' ' + str(row['DD.MM.YYYY'].year) + "\t/\n/\n")
+    file_out.write("WCONHIST\n")
+    file_out.write("\'" + row['WELL'] + "\'" + '\t' + "\'OPEN\'\t\'LRAT\'\t" + str(fRoanding(row['QOIL'])) + '\t' +
+                   str(fRoanding(row['QWAT'])) + '\t' + str(fRoanding(row['QGAS'])) + "\t3*\t" +
+                   str(fRoanding(row['BHP'])) + "\t/\n/\n")
+    #if row['flag']:
+    #file_out.write("/\n")
+    file_out.write("WELTARG\n" + "\'" + str(row['WELL']) + "\'\t" + str(30) + "\t/\n/\n")
+    file_out.write("WEFAC\n" + "\'" + str(row['WELL']) + "\'\t" + str(row['WEFA']) + "\t/\n/\n")
